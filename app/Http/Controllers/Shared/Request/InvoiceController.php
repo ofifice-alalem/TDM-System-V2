6e31c1a3322d6197711fabe8978b21e7279cd17e
ofifice-alalem <ofifice.alalem@gmail.com>
2026-02-21 14:31:11 +0200

    طبات المسوقين من المخزن

--- app/Http/Controllers/Shared/Request/InvoiceController.php
+++ app/Http/Controllers/Shared/Request/InvoiceController.php
@@ -29,7 +29,36 @@ class InvoiceController extends Controller
         ];
 
         $logoPath = public_path('images/company.png');
-        $logoBase64 = file_exists($logoPath) ? base64_encode(file_get_contents($logoPath)) : null;
+        $logoBase64 = null;
+        if (file_exists($logoPath) && is_file($logoPath) && str_starts_with(realpath($logoPath), public_path('images'))) {
+            $image = imagecreatefrompng($logoPath);
+            if ($image) {
+                $width = imagesx($image);
+                $height = imagesy($image);
+                $maxWidth = 200;
+                if ($width > $maxWidth) {
+                    $newWidth = $maxWidth;
+                    $newHeight = ($height / $width) * $newWidth;
+                    $resized = imagecreatetruecolor($newWidth, $newHeight);
+                    imagealphablending($resized, false);
+                    imagesavealpha($resized, true);
+                    $transparent = imagecolorallocatealpha($resized, 255, 255, 255, 127);
+                    imagefilledrectangle($resized, 0, 0, $newWidth, $newHeight, $transparent);
+                    imagecopyresampled($resized, $image, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+                    imagedestroy($image);
+                    $image = $resized;
+                }
+                imagealphablending($image, false);
+                imagesavealpha($image, true);
+                ob_start();
+                imagepng($image, null, 9);
+                $compressedImage = ob_get_clean();
+                imagedestroy($image);
+                $logoBase64 = base64_encode($compressedImage);
+            } else {
+                $logoBase64 = base64_encode(file_get_contents($logoPath));
+            }
+        }
         
         $companyName = $arabic->utf8Glyphs('شركة المتفوقون الأوائل للصناعات البلاستيكية');
 
@@ -68,7 +97,9 @@ class InvoiceController extends Controller
             ->setPaper('a4')
             ->setOption('isRemoteEnabled', false)
             ->setOption('isHtml5ParserEnabled', true)
-            ->setOption('isFontSubsettingEnabled', true);
+            ->setOption('isFontSubsettingEnabled', true)
+            ->setOption('compress', 1)
+            ->setOption('dpi', 96);
         return $pdf->download('request-' . $request->invoice_number . '.pdf');
     }
 }
